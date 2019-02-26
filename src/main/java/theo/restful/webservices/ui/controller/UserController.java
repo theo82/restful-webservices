e6e25b058f615d8aa85b4ce9141aafee4a80e2cb
theo82 <theoaristi53@gmail.com>
2019-02-26 17:41:08 +0200

    Adding UserController class. Closes #1

---
+++ src/main/java/theo/restful/webservices/ui/controller/UserController.java
@@ -0,0 +1,9 @@
+package theo.restful.webservices.ui.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+}
