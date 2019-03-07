d7e5abd4da3c7de2f76dae61794ee902d9c79f09
theo82 <theoaristi53@gmail.com>
2019-03-07 19:33:22 +0200

    Get User Details Resource Method. Closes #28.

--- src/main/java/theo/restful/webservices/ui/controller/UserController.java
+++ src/main/java/theo/restful/webservices/ui/controller/UserController.java
@@ -16,9 +16,15 @@ public class UserController {
     UserService userService;
 
 
-    @GetMapping
-    public String getUser(){
-        return "get user was called";
+    @GetMapping(path = "/{id}")
+    public UserRest getUser(@PathVariable String id){
+
+        UserRest returnValue = new UserRest();
+
+        UserDto userDto = userService.getUserByUserId(id);
+        BeanUtils.copyProperties(userDto, returnValue);
+
+        return returnValue;
     }
 
     @PostMapping
