71c8614bbe4963577e66a3970c48624dcc58772a
theo82 <theoaristi53@gmail.com>
2019-03-13 18:42:43 +0200

    Generating a WAR file. Closes #42

--- src/main/java/theo/restful/webservices/RestfulWebservicesApplication.java
+++ src/main/java/theo/restful/webservices/RestfulWebservicesApplication.java
@@ -2,12 +2,14 @@ package theo.restful.webservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import theo.restful.webservices.security.AppProperties;
 
 @SpringBootApplication
-public class RestfulWebservicesApplication {
+public class RestfulWebservicesApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RestfulWebservicesApplication.class, args);
@@ -27,5 +29,10 @@ public class RestfulWebservicesApplication {
     @Bean(name = "AppProperties")
     public AppProperties appProperties(){
         return new AppProperties();
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RestfulWebservicesApplication.class);
     }
 }
