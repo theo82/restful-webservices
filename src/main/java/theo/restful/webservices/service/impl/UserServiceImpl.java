07dece8f69767995a8bb0eb689be207e76ff8377
theo82 <theoaristi53@gmail.com>
2019-03-31 18:33:29 +0300

    The findUserByEmailVerificationToken() Data Layer Function. Closes #60.

--- src/main/java/theo/restful/webservices/service/impl/UserServiceImpl.java
+++ src/main/java/theo/restful/webservices/service/impl/UserServiceImpl.java
@@ -148,6 +148,27 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    public boolean verifyEmailToken(String token) {
+
+        boolean returnValue = false;
+
+        //Find user by token
+        UserEntity userEntity = userRepository.findUserByEmailVerificationToken(token);
+
+        if (userEntity != null) {
+            boolean hasTokenExpired = Utils.hasTokenExpired(token);
+            if (!hasTokenExpired) {
+                userEntity.setEmailVerificationToken(null);
+                userEntity.setEmailVerificationStatus(Boolean.TRUE);
+                userRepository.save(userEntity);
+                returnValue = true;
+            }
+        }
+
+        return returnValue;
+    }
+
+    @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
         UserEntity userEntity = userRepository.findByEmail(email);
