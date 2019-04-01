c34b6903c5256331ae9cf6e964210a731b20d830
theo82 <theoaristi53@gmail.com>
2019-04-01 17:18:35 +0300

    Checking if Email Verification Token Has Expired. Closes #62.

--- src/main/java/theo/restful/webservices/shared/Utils.java
+++ src/main/java/theo/restful/webservices/shared/Utils.java
@@ -1,8 +1,12 @@
 package theo.restful.webservices.shared;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Component;
+import theo.restful.webservices.security.SecurityConstants;
 
 import java.security.SecureRandom;
+import java.util.Date;
 import java.util.Random;
 
 @Component
@@ -10,6 +14,23 @@ public class Utils {
 
     private final Random RANDOM = new SecureRandom();
     private final String ALPHABET = "0123456789ABCDEDGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuxvxyz";
+
+    /**
+     * A method that will be used to check is a token has expired or not.
+     *
+     * @param token
+     * @return
+     */
+    public static boolean hasTokenExpired(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(SecurityConstants.getTokenSecret())
+                .parseClaimsJws(token).getBody();
+
+        Date tokenExpirationDate = claims.getExpiration();
+        Date todayDate = new Date();
+
+        return tokenExpirationDate.before(todayDate);
+    }
 
     public String generateUserId(int length){
         return generateRandomString(length);
